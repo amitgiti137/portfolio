d9d921de5ff515cb9491b17cd2c1acb18ed4f93d
amitgiti137 <amit1317jmp@gmail.com>
2025-04-29 10:26:08 +0530

    Initial commit

--- app/components/Resume.jsx
+++ app/components/Resume.jsx
@@ -54,7 +54,7 @@ export default function Resume() {
                         </ul>
                     </div>
                 </div>
-                <div className="mt-4 p-6 mb-3 bbg-white dark:bg-gray-800 rounded-xl">
+                <div className="mt-4 p-6 mb-3 bg-white dark:bg-gray-800 rounded-xl">
                     <Skills />
                 </div>
                 <div className="mt-4 p-6 mb-3 bg-white rounded-xl">
