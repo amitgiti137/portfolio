0361a6af749cdbd00a6f3d8b2a22419e89535c8e
amitgiti137 <amit1317jmp@gmail.com>
2025-09-29 23:28:18 +0530

    update

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,6 +1,6 @@
 export default function Contact() {
     return (
-      <section id="contact" className="py-16 bg-gradient-to-r from-[#CBD2A4] via-[#C9E9D2] to-[#E4E0E1] dark:bg-gray-900 rounded-2xl">
+      <section id="contact" className="h-screen py-16 bg-gradient-to-r from-[#CBD2A4] via-[#C9E9D2] to-[#E4E0E1] dark:bg-gray-900 rounded-2xl">
         <div className="max-w-4xl mx-auto px-8 text-center">
           <h2 className="text-4xl font-bold mb-6 text-gray-900 dark:text-white">Contact Me</h2>
           <p className="mb-4 text-gray-800 dark:text-gray-300">📍 Kolkata</p>
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <footer className="py-4 text-center text-gray-500 bg-white dark:bg-gray-400 dark:text-white">
+      <footer className="py-4 h-10 flex justify-center items-center text-gray-500 bg-white dark:bg-gray-400 dark:text-white">
         © 2025 Amit Kumar. All Rights Reserved.
       </footer>
     );
